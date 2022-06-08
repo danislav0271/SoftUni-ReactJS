4eed86599fe3bcef0ae826c15598ec4e749c59ad
Danislav <danislavkostolov@gmail.com>
2022-06-08 15:39:38 +0300

    game play app

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ErrorPage = ({
+    children,
+}) => {
+  return (
+        <section id="catalog-page">
+            <h1>Page Not Found</h1>
+            
+            { children && <h3 className="no-articles">{children}</h3> }
+        </section>  
+    )
+}
+
+export default ErrorPage
---
+++ src/services/gameService.js
@@ -0,0 +1,13 @@
+const baseUrl = 'http://localhost:3030/data';
+
+export function getAll() {
+    return fetch(`${baseUrl}/games?sortBy=_createdOn%20desc`)
+        .then(res => res.json());
+}
+
+export function getOne(id) {
+    return fetch(`${baseUrl}/games/${id}`)
+        .then(res => res.json());
+}
+
+export const getLatest = () => fetch(`${baseUrl}/games?sortBy=_createdOn%20desc&distinct=category`).then(res => res.json());
